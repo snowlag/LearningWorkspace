8441527d8605b253c5de934bbcf9133c4ce904fa
Snowlag <ankitjoshicr7@gmail.com>
2021-03-14 16:20:20 +0530

    Added tree traversal method

--- DS/binarysearchtree.js
+++ DS/binarysearchtree.js
@@ -27,7 +27,7 @@ class BinarySearchTree {
             }   
         }
     }
-    //search in binary tree
+    //search in binary search tree
     search(value){
         if(!this.root) {
             return {found : false}
@@ -50,10 +50,94 @@ class BinarySearchTree {
         }
     }
 
-    //Remove from binary search tree
-    remove(value){
-        
+
+    remove(value){    
+        // search for v
+        if(!this.root) {
+            return {found : false}
+        }
+     
     }
+
+    //Breadth first search
+    BFSsearch(){
+        var queue = [];
+        var visited = []
+        //push the root node in queue
+        queue.push(this.root)
+        while(queue.length != 0){
+            //pop the first element of queue
+            var node = queue.shift()
+            visited.push(node)
+            //if node has left child push in queue
+            if(node.left){
+                queue.push(node.left)
+            }
+            //if node has right child push in queue
+            if(node.right){
+                queue.push(node.right)
+            }
+        }
+        return visited
+    }
+
+    //Depth First Search
+    //1) Prooder search
+    PreoderSearch(){
+        var visited= [];
+        const preoder = (node) =>{
+            visited.push(node)
+            if(node.left){
+                preoder(node.left)
+            }
+            if(node.right){
+                preoder(node.right)
+            }
+        }
+        preoder(this.root)
+        return visited
+
+    }
+
+    //2) Postoder search 
+    PostOderSearch(){
+        var visited= [];
+        const postoder = (node) =>{
+            if(node.left){
+                postoder(node.left)
+            }
+            if(node.right){
+                postoder(node.right)
+            }
+            visited.push(node)
+        }
+        postoder(this.root)
+        return visited
+    }
+
+    //3) Inoder Search
+    InOderSearch(){
+        var visited= [];
+        const inoder = (node) =>{
+            if(node.left){
+                inoder(node.left)
+            }
+            visited.push(node)
+            if(node.right){
+                inoder(node.right)
+            }
+        }
+        inoder(this.root)
+        return visited
+    }
+
+    //
+
+
+
+
+
+
 
 
 }
@@ -67,6 +151,28 @@ class Node{
  
 }
 
+
+
+
 var tree = new BinarySearchTree()
+tree.insert(21)
+tree.insert(25)
+tree.insert(4)
+tree.insert(24)
+tree.insert(13)
+tree.insert(26)
+// console.log(tree.BFSsearch())
+// console.log(tree.PreoderSearch())
+// console.log(tree.PostOderSearch())
+// console.log(tree.InOderSearch())
 
 
+//        21
+//   4        25
+//     13   24  26
+         
+
+//BFS = [21 , 4 , 25 , 13 , 24 , 26]
+//Preoder = [21 , 4 , 13 , 25 ,24 ,  26]
+//postoder = [13 , 4 , 24 , 26 , 25 , 21]
+//InOder = [4 , 13 , 21 , 24 , 25 , 26]
